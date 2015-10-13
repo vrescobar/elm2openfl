0eb679a6770d527c782cf93e0e50fb43cd1a37a2
Víctor R. Escobar <victor.r.e.o@gmail.com>
2015-10-13 23:25:50 +0200

    Added again the clock example

--- Events/time/clock/Source/Main.hx
+++ Events/time/clock/Source/Main.hx
@@ -1,39 +1,66 @@
 package;
 
+import openfl.Lib;
 import elm.graphics.collage.Ngon;
-import openfl.text.TextField;
 import openfl.display.Sprite;
 import openfl.events.Event;
-import Lib;
-import Math.*;
+import openfl.events.TimerEvent;
+import openfl.utils.Timer;
+import Std;
+import Date;
+
 
 class Main extends Sprite {
     private var p:Sprite;
     private var watch:Sprite;
     private var watch_border:Sprite;
+    private var seconds_hand:Sprite;
+    private var minutes_hand:Sprite;
+    private var hours_hand:Sprite;
 
     function new(){
         super();
-        var txt = new TextField();
-        watch = new Ngon(0, 0, 12, 110, Std.random(0xFFFFFF), 0.7);
-        watch_border = new Ngon(0, 0, 12, 111, Std.random(0xFFFFFF), 0.7);
+
+        watch_border = new Ngon(0, 0, 12, 110, 0xEEEEED, 1);
+        watch = new Ngon(0, 0, 12, 111, 0xD6DAD3, 1);
+        seconds_hand = newHand(100, 0xFF0000);
+        minutes_hand = newHand(100, 0x565854);
+        hours_hand = newHand(60, 0x565854);
 
         addChild(watch);
         addChild(watch_border);
-        stage.addEventListener(Event.RESIZE, keepLayout);
+        addChild(seconds_hand);
+        addChild(minutes_hand);
+        addChild(hours_hand);
+
+        stage.addEventListener(Event.RESIZE, onResize);
+        keepLayout();
+
+        var timer = new Timer(1000);
+        timer.addEventListener (TimerEvent.TIMER, timer_onTimer);
+        timer.start();
+    }
+    private function timer_onTimer(event:TimerEvent):Void {
+        var d = Date.now();
+        seconds_hand.rotation = d.getSeconds() * (360/60) - 90;
+        minutes_hand.rotation = d.getMinutes() * (360/60) - 90;
+        hours_hand.rotation = d.getHours() * (360/24) - 90;
     }
 
-    function onSecond() {
-        //p.rotation = Std.random(360);
+    function onResize(e:Event) keepLayout();
+    function keepLayout() {
+        watch.x = Std.int(Lib.current.stage.stageWidth/2);
+        watch.y = Std.int(Lib.current.stage.stageHeight/2);
+        watch_border.x = hours_hand.x = minutes_hand.x = seconds_hand.x = watch.x;
+        watch_border.y = hours_hand.y = minutes_hand.y = seconds_hand.y = watch.y;
     }
-    function keepLayout(e:Event) {
-        center(watch);
+
+    function newHand(radius:Float, color:Int, ?alpha:Float=1) {
+        var hand:Sprite = new Sprite();
+        hand.graphics.beginFill(color);
+        hand.graphics.drawRect(0,0, radius, 0.8);
+        hand.graphics.endFill();
+        return hand;
+
     }
-    function center(s:Sprite) {
-        s.x = Std.int(Lib.current.stage.stageWidth/2) - s.textWidth / 2;
-        s.y = Std.int(Lib.current.stage.stageHeight/2);
-        return s
-    }
-    function onResize(e:Event) init();
 }
-
